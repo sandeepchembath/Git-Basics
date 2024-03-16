cab80a984afa85a7e9b2422ebce2fcbf17dc75fa
Sandeep Chembath <sandeepchembath@gmail.com>
2024-03-16 17:22:39 +0000

    Added git clone description

--- git-basics.py
+++ git-basics.py
@@ -1,1 +1,12 @@
+# Git commands and their descriptions
 
+git_commands = {
+    "git clone": "Makes a local copy of code in a remote repository onto your local machine.",
+    "git add": "",
+    "git commit": "",
+    "git status": "",
+    "git push": "",
+    "git pull": "",
+    "git branch": "",
+    "git merge": "",
+}
