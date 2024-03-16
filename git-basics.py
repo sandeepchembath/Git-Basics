2c05bc1c0601b20add247a94a9fec90456a09d80
Sandeep Chembath <sandeepchembath@gmail.com>
2024-03-16 17:30:52 +0000

    Added more definitions

--- git-basics.py
+++ git-basics.py
@@ -2,10 +2,10 @@
 
 git_commands = {
     "git clone": "Makes a local copy of code in a remote repository onto your local machine.",
-    "git add": "",
-    "git commit": "",
+    "git add": "Adds a file to the staging area.",
+    "git commit": "Saves the changes made to the files in the staging area.",
     "git status": "",
-    "git push": "",
+    "git push": "Copies the changes made to the file in the staging area to the remote repository.",
     "git pull": "",
     "git branch": "",
     "git merge": "",
